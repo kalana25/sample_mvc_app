26745cb66361d046298de96a44dc0f24a0f647df
Kalana Sachith <kal25mahaarachchi@gmail.com>
2022-04-22 12:16:24 +0530

    initail commit

---
+++ App_Deveops/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_Deveops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
